2dec15ffc9376f3a9b73821932719904064ea282
mhmiwamiwaus <mhmiwa@miwa.us>
2023-11-30 19:04:52 -0700

    Added config files and stuff

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d 3-10skillcheck4.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email mhmiwa@miwa.us
+sudo certbot -n -d skillcheck4.3-10.miwauniverse.com --nginx --agree-tos --email mhmiwa@miwa.us
